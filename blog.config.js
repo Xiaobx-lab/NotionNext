5b0ac5b7857ae7d271a6643f8529a74108edf858
Xiaobx-lab <57618171+Xiaobx-lab@users.noreply.github.com>
2024-05-13 19:20:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Sylvia', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个不宅的算法狗', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://vibesylvia.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://vibesylvia.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '技术博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
